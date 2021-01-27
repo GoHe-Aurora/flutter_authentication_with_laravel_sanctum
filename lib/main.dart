647035f3a82f1ffc1fcd7ca45e1de73cf59292e9
GoHe-Aurora <al221911627@gmail.com>
2021-01-27 03:10:13 -0600

    08 Requesting the token in our app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_authentication_with_laravel_sanctum/providers/auth.dart';
+import 'package:flutter_authentication_with_laravel_sanctum/widgets/nav-drawer.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ChangeNotifierProvider(create: (_) => Auth(), child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Sanctum Authentication'),
     );
   }
 }
@@ -29,64 +30,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        drawer: NavDrawer(),
+        body: Center(child: Consumer<Auth>(builder: (context, auth, child) {
+          if (auth.authenticated) {
+            return Text('Haz Iniciado Sesión');
+          } else {
+            return Text('No haz Iniciado Sesión');
+          }
+        })));
   }
 }
---
+++ lib/screen/login-screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_authentication_with_laravel_sanctum/providers/auth.dart';
+import 'package:provider/provider.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return LoginState();
+  }
+}
+
+class LoginState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _email;
+  String _password;
+
+  void submit() {
+    Provider.of<Auth>(context, listen: false).login(credentials: {
+      'email': _email,
+      'password': _password,
+    });
+
+    Navigator.pop(context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Iniciar Sesión'),
+      ),
+      body: Form(
+          key: _formKey,
+          child: Scrollbar(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  TextFormField(
+                    initialValue: 'jairo@gmail.com',
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'example@email.com',
+                    ),
+                    onSaved: (value) {
+                      _email = value;
+                    },
+                  ),
+                  TextFormField(
+                    initialValue: 'password',
+                    decoration: InputDecoration(
+                      labelText: 'Contraseña',
+                    ),
+                    onSaved: (value) {
+                      _password = value;
+                    },
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: FlatButton(
+                      child: Text('Iniciar Sesión'),
+                      onPressed: () {
+                        _formKey.currentState.save();
+
+                        this.submit();
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+}
